50cc2542c87be2894bd2a0f8878327f04abbe441
BitR13x <bitr13x@protonmail.com>
2023-07-06 09:13:15 +0200

    verify functions

--- contracts/Data.sol
+++ contracts/Data.sol
@@ -6,7 +6,10 @@ import "./openzeppelin/contracts/utils/Base64.sol";
 contract Data {
     address payable public owner;
     bool private visibility;
-    string data;
+    string private data;
+
+    // keccak256 hash
+    bytes32 private my_hash;
 
     constructor(string memory _data, bool _visibility) payable {
         require(bytes(data).length > 0, "Invalid data.");
@@ -21,7 +24,15 @@ contract Data {
         owner = payable(msg.sender);
     }
 
-    function encrypt(string memory _pass) public {}
+    function encrypt() public {
+        my_hash = keccak256(abi.encodePacked(data));
+    }
 
-    function verify(string memory _file_string) public view {}
+    function verify_by_data(string memory _data) external view returns (bool) {
+        return keccak256(abi.encodePacked(_data)) == my_hash;
+    }
+
+    function verify_by_hash(bytes32 _hash) external view returns (bool) {
+        return my_hash == _hash;
+    }
 }
