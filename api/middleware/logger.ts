484f0720a95404ddc1963a0b78de1f8500535cd3
BitR13x <bitr13x@protonmail.com>
2023-08-14 16:23:31 +0200

    error logger

---
+++ api/middleware/logger.ts
@@ -0,0 +1,17 @@
+import winston from "winston";
+
+const logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(info => {
+      return `${info.timestamp} - ${info.level}: ${info.message}`;
+    })
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    // Add more transports as needed
+  ],
+});
+
+export default logger;
