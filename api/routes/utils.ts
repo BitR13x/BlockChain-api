70f93c3f21ff4bd19a6bd0bf5868ab0928f62e8c
BitR13x <bitr13x@protonmail.com>
2023-08-21 15:07:14 +0200

    API utils

---
+++ api/routes/utils.ts
@@ -0,0 +1,14 @@
+const getCurrentFormattedTime = () => {
+    let now = new Date();
+    let year = now.getFullYear();
+    let month = String(now.getMonth() + 1).padStart(2, '0');
+    let day = String(now.getDate()).padStart(2, '0');
+    let hours = String(now.getHours()).padStart(2, '0');
+    let minutes = String(now.getMinutes()).padStart(2, '0');
+    let seconds = String(now.getSeconds()).padStart(2, '0');
+    let milliseconds = String(now.getMilliseconds()).padStart(3, '0');
+    
+    return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}Z`;
+};
+
+export { getCurrentFormattedTime }
